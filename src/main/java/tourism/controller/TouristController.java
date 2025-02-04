5f6e1280e8c4f25c098c66cb4a7351522d08e3c1
huw02 <hannibal@ussing.com>
2025-02-04 15:28:36 +0100

    15.28 04-02-2025, hannibal

--- src/main/java/tourism/controller/TouristController.java
+++ src/main/java/tourism/controller/TouristController.java
@@ -1,18 +1,34 @@
 package tourism.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tourism.model.TouristAttraction;
+import tourism.service.TouristService;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("attractions")
 public class TouristController {
+    private final TouristService touristService;
 
-
-    @RequestMapping("attractions")
-    public ResponseEntity<TouristAttraction> getAttractions(){
-        return null;
+    public TouristController (TouristService touristService){
+        this.touristService = touristService;
     }
+
+
+    @GetMapping("")
+    public ResponseEntity<List<TouristAttraction>> getAttractions(){
+    List<TouristAttraction> touristAttractions = touristService.getAttractions();
+    return new ResponseEntity<>(touristAttractions, HttpStatus.OK);
+    }
+
+
+
+
 
 
 }
