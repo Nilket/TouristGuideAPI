238948e02cecface1b954ec095919339d6db28c1
Nilket <joni0010@stud.kea.dk>
2025-02-04 15:09:00 +0100

    Controller gjort klar

--- src/main/java/tourism/controller/TouristController.java
+++ src/main/java/tourism/controller/TouristController.java
@@ -1,7 +1,18 @@
 package tourism.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import tourism.model.TouristAttraction;
 
 @Controller
 public class TouristController {
+
+
+    @RequestMapping("attractions")
+    public ResponseEntity<TouristAttraction> getAttractions(){
+        return null;
+    }
+
+
 }
